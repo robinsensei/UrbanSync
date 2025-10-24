92b01fbb2d20003441c3531146dd38f2c5809ed4
BounceSensei <robinson.domingo11@gmail.com>
2025-10-24 21:16:26 +0800

    fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_PUBLIC_PATH || '/UrbanSync',
   server: {
     port: 5173,
     proxy: {
